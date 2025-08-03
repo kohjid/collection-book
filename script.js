ff20db26df1f4b46621afbd30851d2e7aa9c40e6
kohjid <kouji_piano@mac.com>
2025-08-03 18:17:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ const collectionGrid = document.getElementById('collection-grid');
 
 // これから集めるキャラクターの情報を入れておくための、空のリスト（配列）を用意します。
 let characters = [];
+
+
+
+// 「追加する」ボタンがクリックされたときの処理
+addButton.addEventListener('click', () => {
+    // フォームに入力された値を取得する
+    const name = nameInput.value;
+    const imageUrl = imageInput.value;
+    const description = descInput.value;
+
+    // 新しいキャラクターのデータを作成
+    const newCharacter = {
+        name: name,
+        image: imageUrl,
+        description: description
+    };
+
+    // 作成したキャラクターを、charactersリストに追加
+    characters.push(newCharacter);
+
+    // 確認のために、コンソールに表示してみる
+    console.log('キャラクターが追加されました！');
+    console.log(characters);
+});
